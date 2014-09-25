df8043b2dc191ac0795ccce186432b13be56857d
ansross <ansross@users.noreply.github.com>
2014-09-25 10:50:13 -0600

    change to storing adjacency as boolean vector

--- test/src/k_mer.h
+++ test/src/k_mer.h
@@ -10,17 +10,18 @@
 #include<string>
 #include<vector>
 
+
 class k_mer{
 	private:
 		std::string k_mer_string;
 		enum bases {A=0, C=1, T=2, G=3, N=4};
-		bool adjacent_kmer_end [5] = {false};
+		bool adjacent_kmer_end [5];
 	public:
 	k_mer(void);
 	k_mer(const std::string k_mer_arg);
 	~k_mer(void);
 	void add_adjacent(char adj_arg);
-	bool* get_adj();
+	std::vector<std::string> get_adj();
 	std::string get_k_mer();
 
 };
