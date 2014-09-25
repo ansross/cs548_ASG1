d7103a54ed6ff9a53f367ce6f329e6365b074ae9
ansross <ansross@users.noreply.github.com>
2014-09-25 10:51:45 -0600

    print to file, parse both files

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -20,14 +20,26 @@ int main(int argcv, char* argv[]){
 		const std::string fastq1 = argv[4];
 		const std::string fastq2 = argv[6];
 		kmers all_kmers = kmers();
+		std::cout<<"k = "<<k<<std::endl;
+		std::cout<<"parsing first file "<<fastq1<<std::endl;
 		fastqParser::parseFastq(fastq1, all_kmers, k);
+		std::cout<<"parsing second file "<<fastq2<<std::endl;
+		fastqParser::parseFastq(fastq2, all_kmers, k);
 		std::set<std::string> ordered_kmers = all_kmers.get_all_kmers_ordered();
 		std::set<std::string>::iterator iter;
+		std::string outfile_name = "k_minus_one_mers.txt";
+		std::ofstream outfile;
+		outfile.open(outfile_name);
+		std::cout<<"printing all k-1-mers in lexographical order"<<'\n'<<"k = "<<k<<std::endl;
+		outfile<<"printing all k-1-mers in lexographical order"<<'\n'<<"k = "<<k<<std::endl;
 		for(iter= ordered_kmers.begin(); iter != ordered_kmers.end(); ++iter)
 		{
-			std::cout << *iter << std::endl;	
+			outfile << *iter << std::endl;	
 		}
-		all_kmers.print_all_kmers_with_adj();
+		outfile.close();
+		all_kmers.print_all_kmers_with_adj("k_minus_one_mers_w_adjs.txt");
+
+		return 0;
 	}
 	else{
 		std::cout << "usage: <jarfilename> -k <k> -r1 <fq file 1 name> -r2 <fq file 2 name>";
