6ad73f20f680b4f43c656cf17f9b5a785ce006f3
ansross <ansross@users.noreply.github.com>
2014-09-25 10:48:07 -0600

    Update fastqParser.cpp
    fixed bugs and *hopefully* done!

--- test/src/fastqParser.cpp
+++ test/src/fastqParser.cpp
@@ -8,45 +8,38 @@
 #include "fastqParser.h"
 #include "kmers.h"
 #include <fstream>
-#include <iostream>
-	void fastqParser::parseFastq(const std::string &filename, kmers &data_struct, const int k)
+void fastqParser::parseFastq(const std::string &filename, kmers &data_struct, const int k)
+{
+	std::string line;
+	int lineCount = 0;
+	std::ifstream file(filename);
+	if (file.is_open())
 	{
-		std::string line;
-		int lineCount = 0;
-		std::ifstream file(filename);
-		if (file.is_open())
-		{
-			while( getline(file,line)){
-				if(lineCount%4==1){
-					std::cout<<"line: "<<line<<std::endl;
-					parseLine(line, data_struct, k);
-				}
-				++lineCount;
+		while( getline(file,line)){
+			if(lineCount%4==1){
+				parseLine(line, data_struct, k);
 			}
-			file.close();
+			++lineCount;
 		}
-
-
-
+		file.close();
 	}
-	
-	
 
-	void  fastqParser::parseLine(std::string line, kmers &data_struct, const int k){
-		//getting all (k-1)-mers
-		const int k_minus_one = k-1;
-		std::cout<<"line size: "<<line.size()<<std::endl;
-		for(unsigned i=0; i<line.size()-(k_minus_one); ++i){
-			std::cout<<"kmer: "<<line.substr(i, k-1)<<std::endl;
-			data_struct.add_kmer(line.substr(i,k-1));
-			std::cout<<"i: "<<i<<std::endl;
-			if(i!=0){
-				std::cout<<line.substr(i-1, k-1)<<" adding "<<line.at(i+k-1)<<std::endl;
-				data_struct.add_adjacent(line.substr(i-1,(k-1)), line.at(i+k-1));
-				std::cout<<"added"<<std::endl;
-			}
+
+
+}
+
+
+
+void  fastqParser::parseLine(std::string line, kmers &data_struct, const int k){
+	//getting all (k-1)-mers
+	const int k_minus_one = k-1;
+	for(unsigned i=0; i<=line.size()-(k-1); ++i){
+		data_struct.add_kmer(line.substr(i,k-1));
+		if(i!=0){
+			data_struct.add_adjacent(line.substr(i-1,(k-1)), line.at(i+k-1-1));
 		}
 	}
+}
 
 
 
