c7838e805a7a7a65a92a82f0bacb084cc631b14f
ansross <ansross@users.noreply.github.com>
2014-09-25 10:51:21 -0600

    Update kmers.h

--- test/src/kmers.h
+++ test/src/kmers.h
@@ -12,6 +12,7 @@
 #include <utility>
 #include "k_mer.h"
 #include <iostream>
+#include <fstream>
 class kmers{
 private:
 		std::unordered_map<std::string, k_mer> all_kmers;
@@ -22,7 +23,7 @@ public:
 	void add_kmer(std::string kmer_arg);
 	std::set<std::string> get_all_kmers_ordered(void);
 	void add_adjacent(std::string kmer, char kmer_adj);
-	void print_all_kmers_with_adj(void);
+	void print_all_kmers_with_adj(std::string outfile_name);
 };
 
 
