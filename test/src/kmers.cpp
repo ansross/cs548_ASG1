ac1279faf132a7e69c2873d05265cfa471504baa
ansross <ansross@users.noreply.github.com>
2014-09-25 10:51:07 -0600

    updated kmers.cpp
     and changed from header file...oops!

--- test/src/kmers.cpp
+++ test/src/kmers.cpp
@@ -1,31 +1,52 @@
-/*
- * kmers.h
- *
- *  Created on: Sep 21, 2014
- *      Author: anross
- */
+#include "kmers.h"
 
-#ifndef KMERS_H_
-#define KMERS_H_
-#include <set>
-#include <unordered_map>
-#include <utility>
-#include "k_mer.h"
-#include <iostream>
-#include <fstream>
-class kmers{
-private:
-		std::unordered_map<std::string, k_mer> all_kmers;
+kmers::kmers(void){
+	
+}
 
-public:
-	kmers(void);
-	~kmers(void);
-	void add_kmer(std::string kmer_arg);
-	std::set<std::string> get_all_kmers_ordered(void);
-	void add_adjacent(std::string kmer, char kmer_adj);
-	void print_all_kmers_with_adj(std::string outfile_name);
-};
+kmers::~kmers(void){
+}
+
+void kmers::add_kmer(std::string kmer_string_arg){
+	k_mer new_kmer = k_mer(kmer_string_arg);
+	std::pair<std::string, k_mer> temp;
+	temp = std::make_pair(kmer_string_arg, new_kmer);
+	all_kmers.insert(temp);
+}
+
+std::set<std::string> kmers::get_all_kmers_ordered(void)
+{
+	std::set<std::string> sorted_kmers = std::set<std::string>();
+	for(auto it = all_kmers.begin(); it != all_kmers.end(); ++it)
+	{
+		sorted_kmers.insert(it->first);
+	}
+	return sorted_kmers;
+}
+
+void kmers::print_all_kmers_with_adj(std::string outfile_name)
+{
+	std::ofstream outfile;
+	outfile.open(outfile_name);
+	std::cout<<"printing all k-1-mers with adjacencies"<<std::endl;
+	outfile<<"printing all k-1-mers with adjacencies"<<std::endl;
+	std::set<std::string> sorted_kmers = kmers::get_all_kmers_ordered();
+	for(auto iter = sorted_kmers.begin(); iter!=sorted_kmers.end(); ++iter)
+	{
+		outfile << *iter <<": ";
+		std::vector<std::string> adjs = all_kmers[*iter].get_adj();
+		for(auto i = adjs.begin(); i != adjs.end(); ++i)
+		{
+			outfile<<*i<<", ";
+		}
+		outfile<<std::endl;
+	}
+	outfile.close();
+}
+
+void kmers::add_adjacent(std::string kmer, char kmer_adj){
+	all_kmers[kmer].add_adjacent(kmer_adj);
+}
 
 
 
-#endif /* KMERS_H_ */
